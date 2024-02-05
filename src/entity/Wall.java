217bc8585f91f9a7bdf523fd95b185bacd950154
Greets <34511287+Louis-Vauterin@users.noreply.github.com>
2024-02-05 21:55:06 +0100

    getType for wall

--- src/entity/Wall.java
+++ src/entity/Wall.java
@@ -4,4 +4,10 @@ import com.modeliosoft.modelio.javadesigner.annotations.objid;
 
 @objid ("7e4794ac-4e29-491c-bce7-44c2e36a2a6b")
 public class Wall extends Square {
+	
+	@Override
+	String getType() {
+		
+		return "Wall";
+	}
 }
