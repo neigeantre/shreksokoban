6320ba044330a45d67e17be82c1d3ec9d6021730
Greets <34511287+Louis-Vauterin@users.noreply.github.com>
2024-02-05 22:31:21 +0100

    implement getType for stone

--- src/entity/Stone.java
+++ src/entity/Stone.java
@@ -4,4 +4,11 @@ import com.modeliosoft.modelio.javadesigner.annotations.objid;
 
 @objid ("ea2e33f9-dec1-48ee-bea1-93eccafc46c7")
 public class Stone extends Moveable {
+	
+	
+    @Override
+    String getType() {
+    	return "Stone";
+    }
+
 }
