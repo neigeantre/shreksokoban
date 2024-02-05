e6ce631cf6b1164c945afca879bad339654eab78
Greets <34511287+Louis-Vauterin@users.noreply.github.com>
2024-02-05 21:22:01 +0100

    add getters for warehouse

--- src/entity/Warehouse.java
+++ src/entity/Warehouse.java
@@ -7,9 +7,21 @@ import com.modeliosoft.modelio.javadesigner.annotations.objid;
 @objid ("c8e0e928-61bf-4b53-8d1e-88b44e44d04a")
 public class Warehouse {
     @objid ("ee3812eb-85c9-4ff5-af1c-acd6139cdfae")
-    private List<Position> positions = new ArrayList<Position> ();
+    private ArrayList<Position> positions = new ArrayList<Position> ();
 
     @objid ("1fe578cf-6887-44d2-9332-dc8dcb8978ab")
-    private List<Square> squares = new ArrayList<Square> ();
-
+    private ArrayList<Square> squares = new ArrayList<Square> ();
+    
+    public Warehouse(ArrayList<Position> positions, ArrayList<Square> squares) {
+    	this.positions = positions;
+    	this.squares = squares;
+    }
+    
+    public ArrayList<Square> getSquares(){
+    	return this.squares;
+    }
+    
+    public ArrayList<Position> getPositions(){
+    	return this.positions;
+    }
 }
