83233c28320ffbde5f8d10bc9e94fe9144f43674
Greets <34511287+Louis-Vauterin@users.noreply.github.com>
2024-02-05 22:31:53 +0100

    implement getType for player

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -4,4 +4,10 @@ import com.modeliosoft.modelio.javadesigner.annotations.objid;
 
 @objid ("a6fd0137-8e93-43cc-902c-d5e1b6eefc65")
 public class Player extends Moveable {
+	
+	
+    @Override
+    String getType() {
+    	return "Player";
+    }
 }
