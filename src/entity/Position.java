ba546db0f6849e0a3a877cb70cbc67bd86918b14
arthur <arthur.bougeard@student-cs.fr>
2024-02-06 09:45:00 +0100

    Position class

--- src/entity/Position.java
+++ src/entity/Position.java
@@ -1,19 +1,55 @@
 package entity;
+import java.util.ArrayList;
 
 import com.modeliosoft.modelio.javadesigner.annotations.objid;
 
 @objid ("cf75c6ab-9e29-46d7-9507-29e1dac0aea6")
 public class Position {
-    @objid ("d3741440-f645-4d0a-85e3-fd5f86b68659")
+	
+	public static ArrayList<Position> generateGrid(int height, int width, Warehouse warehouse){
+		ArrayList<Position> grid = new ArrayList<>(); 
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+				grid.add(new Position(i, j));
+			}
+		}
+		return grid;
+	}
+	
+	public Position(int row, int column) {
+		this.row = row;
+		this.column = column;
+		this.square = null;
+	}
+	
+    public Position(int row, int column, Square square) {
+		this.row = row;
+		this.column = column;
+		this.square = square;
+	}
+
+	@objid ("d3741440-f645-4d0a-85e3-fd5f86b68659")
     private int row;
 
     @objid ("bf62665c-6b4b-405e-91dd-76ff8d6ebf26")
     private int column;
 
-    @objid ("505d80c0-920b-4ff1-9645-e949a9d61734")
-    private Warehouse warehouse;
-
     @objid ("559b7e66-8c12-418e-aaf2-a1d5a38ab39a")
     private Square square;
 
+	public Square getSquare() {
+		return square;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+	
+	public String toString() {
+		return "(" + row + ", " + column + ")";
+	}
 }
