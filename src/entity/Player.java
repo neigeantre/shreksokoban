4809143e725a0499ac01572adf25d6ad377889c5
arthur <arthur.bougeard@student-cs.fr>
2024-02-06 09:44:17 +0100

    Player class

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -4,4 +4,20 @@ import com.modeliosoft.modelio.javadesigner.annotations.objid;
 
 @objid ("a6fd0137-8e93-43cc-902c-d5e1b6eefc65")
 public class Player extends Moveable {
+	
+    public Player(Floor floor) {
+		super(floor);
+		// TODO Auto-generated constructor stub
+	}
+
+	@objid ("10549e0f-6a02-4a25-9749-de59b724f856")
+    public void move(final Direction direction) {
+    	if (this.floor.isNeighbourFree(direction)) {
+    		Square neighbour = this.floor.getNeighbour(direction);
+    		assert (neighbour.getClass() == Floor.class);
+    		this.floor = (Floor) neighbour;
+    	}
+    	
+    }
+
 }
