49a86eead97ed931d254ffb8a77924cc8ce2d3ba
arthur <arthur.bougeard@student-cs.fr>
2024-02-06 09:45:35 +0100

    Stone class

--- src/entity/Stone.java
+++ src/entity/Stone.java
@@ -4,4 +4,13 @@ import com.modeliosoft.modelio.javadesigner.annotations.objid;
 
 @objid ("ea2e33f9-dec1-48ee-bea1-93eccafc46c7")
 public class Stone extends Moveable {
+	
+	public Stone(Floor floor) {
+		super(floor);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void setFloor(Floor floor) {
+		this.floor = floor;
+	}
 }
