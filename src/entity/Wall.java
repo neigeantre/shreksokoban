1c0bea06834027b4fa4b05a053f84294ae458b45
arthur <arthur.bougeard@student-cs.fr>
2024-02-06 09:45:47 +0100

    Wall class

--- src/entity/Wall.java
+++ src/entity/Wall.java
@@ -4,4 +4,16 @@ import com.modeliosoft.modelio.javadesigner.annotations.objid;
 
 @objid ("7e4794ac-4e29-491c-bce7-44c2e36a2a6b")
 public class Wall extends Square {
+	public Wall(Warehouse warehouse, Position position) {
+		super(warehouse, position);
+		// TODO Auto-generated constructor stub
+	}
+
+	public boolean isPartiallyFree(Direction direction) {
+		return false;
+	}
+	
+	public boolean isCompletelyFree() {
+		return false;
+	}
 }
