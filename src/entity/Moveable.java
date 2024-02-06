303021630e40a38f23e2446a800f52fc725a48cd
arthur <arthur.bougeard@student-cs.fr>
2024-02-06 09:44:42 +0100

    Moveable class

--- src/entity/Moveable.java
+++ src/entity/Moveable.java
@@ -4,7 +4,15 @@ import com.modeliosoft.modelio.javadesigner.annotations.objid;
 
 @objid ("2db2a328-bb2e-45da-a016-2fb09771ef3e")
 public abstract class Moveable {
-    @objid ("b39ec76f-4022-4113-b29f-6e5ccc978144")
-    private Floor floor;
+    public Moveable(Floor floor) {
+		this.floor = floor;
+	}
+
+	@objid ("b39ec76f-4022-4113-b29f-6e5ccc978144")
+    protected Floor floor;
+
+	public void setFloor(Floor floor) {
+		this.floor = floor;
+	}
 
 }
